e149581f5a694feab5efbd0ba75acb6159412209
Erikvdv <erikvandeven@outlook.com>
2021-10-29 19:07:28 +0200

    update formatting

--- src/Api/Controllers/ProfilesController.cs
+++ src/Api/Controllers/ProfilesController.cs
@@ -5,33 +5,32 @@ using Microsoft.AspNetCore.Mvc;
 using Realworlddotnet.Api.Models;
 using Realworlddotnet.Core.Dto;
 
-namespace Realworlddotnet.Api.Controllers
+namespace Realworlddotnet.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProfilesController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ProfilesController : ControllerBase
+    [HttpGet("{username}")]
+    public async Task<ActionResult<ProfilesEnvelope<Profile>>> GetProfileAsync(string username)
     {
-        [HttpGet("{username}")]
-        public async Task<ActionResult<ProfilesEnvelope<Profile>>> GetProfileAsync(string username)
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
 
-        [Authorize]
-        [HttpPost("{username}/follow")]
-        public async Task<ActionResult<UserEnvelope<UserDto>>> FollowUserAsync(string username)
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
+    [Authorize]
+    [HttpPost("{username}/follow")]
+    public async Task<ActionResult<UserEnvelope<UserDto>>> FollowUserAsync(string username)
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
 
-        [Authorize]
-        [HttpDelete("{username}/follow")]
-        public async Task<ActionResult<UserEnvelope<UserDto>>> UnfollowUserAsync(string username)
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
+    [Authorize]
+    [HttpDelete("{username}/follow")]
+    public async Task<ActionResult<UserEnvelope<UserDto>>> UnfollowUserAsync(string username)
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
     }
 }
